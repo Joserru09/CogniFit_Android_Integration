585c07cd64e668ab0dd02ad6956bdd7070c1c4c5
Joserru09 <75275032+Joserru09@users.noreply.github.com>
2024-06-20 10:59:07 +0000

    cambiosCongfigg.java

---
+++ Integration_CogniFit/Android_CogniFit_Integration/app/src/main/java/com/cf/es/android_cognifit_integration/Config.java
@@ -0,0 +1,7 @@
+package com.cf.es.android_cognifit_integration;
+
+public class Config {
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String USER_TOKEN = "";
+}
