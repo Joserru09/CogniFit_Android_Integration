7b70de59b3f15c23c2d7e9e411d5de69a0bcc742
Joserru09 <75275032+Joserru09@users.noreply.github.com>
2024-06-20 12:50:53 +0200

    Create config.java

---
+++ Integration_CogniFit/Android_CogniFit_Integration/app/src/main/java/com/cf/es/android_cognifit_integration/config.java
@@ -0,0 +1,7 @@
+package com.cf.es.android_cognifit_integration;
+
+public class Config {
+    public static final String CLIENT_ID = "b76cd92b3aa08ac0fe2706c9d8c9d744";
+    public static final String CLIENT_SECRET = "78fccdc9f7dec856b7ea6586d71424b2";
+    public static final String USER_TOKEN = "DQfs3xC7foFcdFovP/EgOsAyTuwEZM6PA6+xATM0tDTzT+LemlM/WZtUFlHuB3ytRA/a2fnqDB5Acsf5uyMDgg==";
+}
