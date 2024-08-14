f1cb3f6a0bc2587f24787c950359b8d8d5f3e953
Joserru09 <75275032+Joserru09@users.noreply.github.com>
2024-08-14 08:56:29 +0000

    changes referal to partners platform

--- Integration_CogniFit/Android_CogniFit_Integration/app/src/main/java/com/cf/es/android_cognifit_integration/Config.java
+++ Integration_CogniFit/Android_CogniFit_Integration/app/src/main/java/com/cf/es/android_cognifit_integration/Config.java
@@ -5,3 +5,6 @@ public class Config {
     public static final String CLIENT_SECRET = ""; // Replace with a valid client secret (In your account, API keys section)
     public static final String USER_TOKEN = ""; // Obtained making an api call,see the readme file for more info
 }
+
+
+// See cognifit.com/developers for more
